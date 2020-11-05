a1021e2b048507ae991e35030bd39e6cdd529db9
himchawla <58020143+himchawla@users.noreply.github.com>
2020-11-05 18:21:13 +1300

    Timerrrrr

---
+++ Assets/Scripts/timerSet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timerSet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int time = (int)GameObject.FindGameObjectWithTag("gameManager").GetComponent<gameManagerScript>().getTimer();
+        GetComponent<TMPro.TextMeshProUGUI>().text = time.ToString();
+    }
+}
